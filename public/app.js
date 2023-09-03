b1d4e6ba67bcaea0bcc52d78d949673fe493757d
Iamneo <techadmin@iamneo.ai>
2023-09-03 06:23:27 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,95 +1,100 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
+// Define variables to keep track of game state
 let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+let isGameActive = true;
+const cells = document.querySelectorAll('.btn');
+const resultContainer = document.querySelector('.result');
+const resetButton = document.querySelector('#reset-button');
+
+// Winning combinations
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 // Function to handle a player's move
 function handleMove(event) {
-    const cell = event.target;
-  
-    // Check if the cell is empty and the game is active
-    if (isGameActive && cell.value === '') {
-      cell.value = currentPlayer;
-      cell.disabled = true;
-      cell.classList.add('played');
-  
-      // Check for a win or a draw
-      if (checkWin()) {
-        endGame(`Player ${currentPlayer} wins!`);
-      } else if (checkDraw()) {
-        endGame("It's a draw!");
-      } else {
-        // Switch to the other player
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        updateResult();
-      }
-    }
-  }
-  
-  // Function to check for a win
-  function checkWin() {
-    for (const combination of winningCombinations) {
-      const [a, b, c] = combination;
-      if (
-        cells[a].value &&
-        cells[a].value === cells[b].value &&
-        cells[a].value === cells[c].value
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  // Function to check for a draw
-  function checkDraw() {
-    return [...cells].every((cell) => cell.value !== '');
-  }
-  
-  // Function to end the game
-  function endGame(message) {
-    isGameActive = false;
-    cells.forEach((cell) => {
-      cell.disabled = true;
-    });
-    resultContainer.textContent = message;
-    resetButton.disabled = false;
-  }
-  
-  // Function to reset the game
-  function resetGame() {
-    isGameActive = true;
-    currentPlayer = 'X';
-    cells.forEach((cell) => {
-      cell.value = '';
-      cell.disabled = false;
-      cell.classList.remove('played');
-    });
-    resultContainer.textContent = "Player X's turn";
-    resetButton.disabled = true;
-  }
-  
-  // Function to update the result display
-  function updateResult() {
-    resultContainer.textContent = `Player ${currentPlayer}'s turn`;
-  }
+  const cell = event.target;
 
- 
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
+  // Check if the cell is empty and the game is active
+  if (isGameActive && cell.value === '') {
+    cell.value = currentPlayer;
+    cell.disabled = true;
+    cell.classList.add('played');
+
+    // Check for a win or a draw
+    if (checkWin()) {
+      endGame(`Player ${currentPlayer} wins!`);
+    } else if (checkDraw()) {
+      endGame("It's a draw!");
+    } else {
+      // Switch to the other player
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      updateResult();
+    }
+  }
+}
+
+// Function to check for a win
+function checkWin() {
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (
+      cells[a].value &&
+      cells[a].value === cells[b].value &&
+      cells[a].value === cells[c].value
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Function to check for a draw
+function checkDraw() {
+  return [...cells].every((cell) => cell.value !== '');
+}
+
+// Function to end the game
+function endGame(message) {
+  isGameActive = false;
+  cells.forEach((cell) => {
+    cell.disabled = true;
+  });
+  resultContainer.textContent = message;
+  resetButton.disabled = false;
+}
+
+// Function to reset the game
+function resetGame() {
+  isGameActive = true;
+  currentPlayer = 'X';
+  cells.forEach((cell) => {
+    cell.value = '';
+    cell.disabled = false;
+    cell.classList.remove('played');
+  });
+  resultContainer.textContent = "Player X's turn";
+  resetButton.disabled = true;
+}
+
+// Function to update the result display
+function updateResult() {
+  resultContainer.textContent = `Player ${currentPlayer}'s turn`;
+}
+
+// Event listeners for cell clicks
+cells.forEach((cell) => {
+  cell.addEventListener('click', handleMove);
 });
 
-document.querySelector('#reset').addEventListener('click', resetGame);
+// Event listener for the Reset button
+resetButton.addEventListener('click', resetGame);
 
+// Initialize the game
 resetGame();
